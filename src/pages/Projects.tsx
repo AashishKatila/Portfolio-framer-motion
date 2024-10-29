c5365dfd4f44c417a5c28eb639ab96b146608ecc
Aashish Katila <aashishkatila@gmail.com>
2024-10-29 12:18:24 +0545

    Fix: Add new projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,13 +2,13 @@ import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 
 import BoldCopy from "../components/animata/text/bold-copy";
-import Pokemon from "../assets/pokemon.png";
-import Ecommerce from "../assets/ecommerce.png";
-import Typing from "../assets/typingtest.png";
-import Movie from "../assets/movieList.png";
-import Weather from "../assets/weather.jpg";
-import Note from "../assets/note.png";
 import Card from "../components/animata/cards/Card";
+import Animata from "../assets/animata.png";
+import Sisu from "../assets/sisu.jpg";
+import Ecommerce from "../assets/ecom.png";
+import Note from "../assets/note.png";
+import KGH from "../assets/KGH.png";
+import MovieList from "../assets/movieList.png";
 
 const gridContainerVariants = {
   hidden: { opacity: 0 },
@@ -29,40 +29,50 @@ const Projects = () => {
       id: 1,
       title: "Animata",
       name: "Animata",
-      description:
+      small_desc:
         "Free & open-source collection of animations, effects, & interactions",
-      image: Note,
+      description:
+        "Animata is a free and open-source collection of hand-crafted animations, effects, and interactions that you can seamlessly integrate into your project with a simple copy and paste.",
+      image: Animata,
       link: "https://animata.design/",
     },
     {
       id: 2,
       title: "Sisu",
       name: "Sisu",
-      description: "An educational companion for young learners",
-      image: Pokemon,
+      small_desc: "An educational companion for young learners",
+      description:
+        "Sisu is an immersive app designed to make learning alphabets, numbers, colors, and basic concepts an exciting adventure.",
+      image: Sisu,
       link: "https://play.google.com/store/apps/details?id=com.codse.sisu&hl=en",
     },
     {
       id: 3,
       title: "Kanti Girls Hostel",
       name: "Kanti Girls Hostel",
-      description: "Kanti Girls Hostel - best girls hostel in Pokhara",
-      image: Typing,
+      small_desc: "Kanti Girls Hostel - best girls hostel in Pokhara",
+      description:
+        "Girls hostel that offers a secure and cozy environment designed for female students and professionals",
+      image: KGH,
       link: "https://kanti-girls-hostel.vercel.app/",
     },
     {
       id: 4,
       title: "Movie List",
       name: "Movie List",
-      description: "Add your favourite movies to holdlist & watched list.",
-      image: Movie,
+      small_desc: "Add your favourite movies to holdlist & watched list.",
+      description:
+        "Save your movies in holdlist and watched list. Uses Context API as global state management.",
+      image: MovieList,
       link: "https://movie-list-pi-five.vercel.app/",
     },
     {
       id: 5,
       title: "E-commerce",
       name: "E-commerce",
-      description: "Simple e-commerce application",
+      small_desc: "E-commerce with add/remove items & search",
+      description:
+        "Add items to your cart, remove them, checkout items, search items. Uses Context API as global state management.",
       image: Ecommerce,
       link: "https://ecommerce-geektech.netlify.app/",
     },
@@ -70,7 +80,9 @@ const Projects = () => {
       id: 6,
       title: "Note App",
       name: "Note App",
-      description: "Note with add, search and delete functionality.",
+      small_desc: "Note with add, search & delete functionality.",
+      description:
+        "Responsive note app where you can add your notes, edit them, delete them. It also have search feature and toggle mode.",
       image: Note,
       link: "https://react-note-save.netlify.app/",
     },
@@ -94,7 +106,9 @@ const Projects = () => {
             id={project.id}
             title={project.title}
             image={project.image}
+            small_desc={project.small_desc}
             description={project.description}
+            link={project.link}
           />
         ))}
       </motion.div>
